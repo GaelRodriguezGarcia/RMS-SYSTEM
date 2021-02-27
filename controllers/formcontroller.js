906c0d9986a242b1b086baf2a2bb83f8dbbddb41
Gael <grodriguez236@west-mec.org>
2021-02-26 18:19:53 -0700

    bit of everything
    added populate db and added more to the controllers and routes

--- controllers/formcontroller.js
+++ controllers/formcontroller.js
@@ -1,6 +1,15 @@
 var form = require('/form.js');
 
 //display the list of the forms
+exports.form_page = function(req, res, next){
+    form.find()
+        .exec(function (err, form_page) {
+            if (err) { return next(err); }
+            //Successful, so render
+            res.render('page_form', { title: 'Form page', form_page: page_form });
+        });
+
+};
 
 
 
