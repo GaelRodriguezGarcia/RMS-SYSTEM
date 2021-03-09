63d269ec4be22decc210b63a189fa5e9fd9b9037
Gael Rodriguez <gael.rod2330@gmail.com>
2021-03-09 15:41:50 -0700

    routing and mongodb added

--- routes/catalog.js
+++ routes/catalog.js
@@ -22,6 +22,52 @@ router.post('manage_form', function(req,res){
 })
 
 
+//
+
+router.get('/getdata', function(req,res){
+  var resultArray = [];
+  mongo.connect(url, function(err, db){
+    assert.equal(null, err);
+    var cursor = db.collection('forms').find();
+    cursor.forEach(function(doc, err){
+      assert.equal(null, err);
+      result.Array.push(doc);
+    }, function(){
+      db.close;
+      res.render('/', {items: resultArray});
+    });
+
+  });
+});
+
+
+router.post('/insert', function(req, res){
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+  mongo.connect(url, function(err, db){
+    WebAuthnAssertion.equal(null, err);
+    db.collection('forms').insertOne(item, function(err, result){
+      assert.equal(null, err);
+      console.log('item inserted');
+      db.close();
+    });
+  });
+
+  res.redirect('/')
+});
+
+router.post('/update', function(req, res){
+  
+})
+
+router.post('/delete', function(req, res){
+  
+})
+
+
 
 
 
